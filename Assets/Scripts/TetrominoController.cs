6d5defbc8ace0496280efb98620b6b4ba7080bc0
Hung <mw3tv123@gmail.com>
2020-03-22 14:40:42 +0700

    Added limitation and allow ration to tetromino.

--- Assets/Scripts/TetrominoController.cs
+++ Assets/Scripts/TetrominoController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class TetrominoController : MonoBehaviour
 {
+    Vector3 rotationAngle = new Vector3(0, 0, 90);
+
     float fall = 0f;
     public float fallSpeed = 1f;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
 
     // Start is called before the first frame update
     void Start() {
@@ -18,24 +23,34 @@ public class TetrominoController : MonoBehaviour
     }
 
     void CheckUserInput() {
+        // Move tetromino to the right.
         if ( Input.GetKeyDown(KeyCode.RightArrow) ) {
             transform.position += new Vector3(1, 0, 0);
             
             if ( !IsValidPosition() )
                 transform.position += new Vector3(-1, 0, 0);
         }
+        // Move tetromino to the left.
         else if ( Input.GetKeyDown(KeyCode.LeftArrow) ) {
             transform.position += new Vector3(-1, 0, 0);
 
             if ( !IsValidPosition() )
                 transform.position += new Vector3(1, 0, 0);
         }
-        else if ( Input.GetKeyDown(KeyCode.UpArrow) ) {
-            transform.Rotate(0, 0, 90);
+        // Rotation tetromino.
+        else if ( Input.GetKeyDown(KeyCode.UpArrow) && allowRotation ) {
+            // Tetromino like Z and I only need to rotate in range of (0 -> 90) degree.
+            if ( limitRotation )
+                rotationAngle.z = transform.rotation.eulerAngles.z >= 90 ? -90 : 90;
 
+            // Rotate this tetromino.
+            transform.Rotate(rotationAngle);
+
+            // If tetromino is out of grid border, rotate back to original position.
             if ( !IsValidPosition() )
-                transform.Rotate(0, 0, -90);
+                transform.Rotate(0, 0, -rotationAngle.z);
         }
+        // Move tetromino down.
         else if ( Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed ) {
             transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
