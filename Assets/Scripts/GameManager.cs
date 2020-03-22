eb63eec79dcd6145d9fa866441df6a4ee07a7b75
Hung <mw3tv123@gmail.com>
2020-03-22 11:11:08 +0700

    Added grid limitation to tetromino.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,13 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool CheckInsideGrid ( Vector2 position ) {
+        return ((int)position.x >= 0 && (int)position.x < gridWidth && (int)position.y >= 0);
+    }
+
+    public Vector2 Round ( Vector2 position ) {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
     }
 }
--- Assets/Scripts/TetrominoController.cs
+++ Assets/Scripts/TetrominoController.cs
@@ -20,16 +20,38 @@ public class TetrominoController : MonoBehaviour
     void CheckUserInput() {
         if ( Input.GetKeyDown(KeyCode.RightArrow) ) {
             transform.position += new Vector3(1, 0, 0);
+            
+            if ( !IsValidPosition() )
+                transform.position += new Vector3(-1, 0, 0);
         }
         else if ( Input.GetKeyDown(KeyCode.LeftArrow) ) {
             transform.position += new Vector3(-1, 0, 0);
+
+            if ( !IsValidPosition() )
+                transform.position += new Vector3(1, 0, 0);
         }
         else if ( Input.GetKeyDown(KeyCode.UpArrow) ) {
             transform.Rotate(0, 0, 90);
+
+            if ( !IsValidPosition() )
+                transform.Rotate(0, 0, -90);
         }
         else if ( Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed ) {
             transform.position += new Vector3(0, -1, 0);
             fall = Time.time;
+
+            if ( !IsValidPosition() )
+                transform.position += new Vector3(0, 1, 0);
         }
+    }
+
+    bool IsValidPosition () {
+        foreach ( Transform mino in transform ) {
+            Vector2 position = FindObjectOfType<GameManager>().Round(mino.position);
+
+            if ( !FindObjectOfType<GameManager>().CheckInsideGrid(position) )
+                return false;
+        }
+        return true;
     }
 }
